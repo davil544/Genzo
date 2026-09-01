5c9b5deb9198aa186c0add90db5d86dbadaf33c3
Dylan A <37839398+davil544@users.noreply.github.com>
2026-08-31 22:29:43 -0400

    Version 1.0.0 ready for production ~Cleaned up CMakePresets to have separate debug and release profiles on Linux and macOS ~macOS builds now generate .app bundles ~Added app icon for macOS builds ~Cleaned up license formatting to increase readibility in About window ~Added README.md ~Updated kvazaar to version 2.3.2 ~Updated libheif to version 1.23.2

--- overlay-ports/libheif/portfile.cmake
+++ overlay-ports/libheif/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO strukturag/libheif
-    REF "v1.23.1"
-    SHA512 ebb0bf74d6d1ae2d39a7cefcec0f4a04006fee330e147c49c863f6d4febb45e82ccebf36cc4e30812bd3591918ee8964fbde9a3a4793c5f60eaf30a0e011add0
+    REF "v1.23.2"
+    SHA512 d34ccd40d7f7cc48ff8ceddafd8f7c7004ab0df2a657237fcc715c4bcc453a41946d7db542d32b781875868ee116c451b4b63daaf073530245ec7b117ae60e27
     HEAD_REF main
 )
 
